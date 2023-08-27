8ea2564b27db04d24367927cfeb045ac9259518e
tacob <tacob@LeoMartinPC.unl.edu>
2023-08-27 12:24:53 -0500

    Here it is

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,7 @@
 public class hello {
 	public static void main(String args[]) {
 		System.out.println("Alex Robinson");
+		System.out.println("Leo Martin");
+		System.out.println("Computer Science");
 	}
 }
